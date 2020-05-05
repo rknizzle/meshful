f7064e45215ad67b1f0ca2e08b6ac1191d3c0880
rknizzle <ryan@autotiv.com>
2020-05-05 18:25:25 -0400

    Added a triangle type

---
+++ triangle.go
@@ -0,0 +1,13 @@
+package main
+
+// A triangle is made up of 3 vertices and has a surface normal
+type Triangle struct {
+	// the 3 vertices that make up a facet
+	Vertices [3]Vec3
+
+	// triangle normal vector
+	Normal Vec3
+
+	// extra info stored only in stl binary files
+	Extra uint16
+}
