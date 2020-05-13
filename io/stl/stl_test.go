3d650def8551bc796c2983031c3747b1e75514ca
rknizzle <ryan@autotiv.com>
2020-05-12 21:50:47 -0400

    Added skeleton of tests for stl reading

---
+++ io/stl/stl_test.go
@@ -0,0 +1,15 @@
+package stl
+
+import (
+	"testing"
+)
+
+// test that reading in a valid STL file returns a proper mesh
+func TestReadValidStlFile(t *testing.T) {
+	t.Error("Not yet implemented")
+}
+
+// test that an invalid STL file will throw an error
+func TestErrorOnInvalidStlFile(t *testing.T) {
+	t.Error("Not yet implemented")
+}
