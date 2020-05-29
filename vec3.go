5e4835566f3e8277501f6a736109e1a3d4968963
rknizzle <ryan@autotiv.com>
2020-05-28 23:54:05 -0400

    Added vec3 function to get cross product of 2 vectors

--- vec3.go
+++ vec3.go
@@ -4,3 +4,12 @@ package meshful
 type Vec3 struct {
 	X, Y, Z float32
 }
+
+// get the cross product of two vectors
+func (vec Vec3) Cross(other Vec3) Vec3 {
+	return Vec3{
+		vec.Y*other.Z - vec.Z*other.Y,
+		vec.Z*other.X - vec.X*other.Z,
+		vec.X*other.Y - vec.Y*other.X,
+	}
+}
