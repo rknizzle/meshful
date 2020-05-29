efe7d0bc80b94f7d46864140754aac7a51e6754b
rknizzle <ryan@autotiv.com>
2020-05-29 18:41:50 -0400

    Added a funcion to get the area of a triangle

--- triangle.go
+++ triangle.go
@@ -1,5 +1,9 @@
 package meshful
 
+import (
+	"math"
+)
+
 // A triangle is made up of 3 vertices and has a surface normal
 type Triangle struct {
 	// the 3 vertices that make up a facet
@@ -14,4 +18,12 @@ type Triangle struct {
 
 func (t *Triangle) GetSignedVolume() float32 {
 	return float32(t.Vertices[0].Dot(t.Vertices[1].Cross(t.Vertices[2])) / 6.0)
+}
+
+func (t *Triangle) GetArea() float32 {
+	ab := t.Vertices[0].Diff(t.Vertices[1])
+	ac := t.Vertices[0].Diff(t.Vertices[2])
+	cross := ab.Cross(ac)
+
+	return float32(0.5 * math.Sqrt(float64(cross.X*cross.X+cross.Y*cross.Y+cross.Z*cross.Z)))
 }
