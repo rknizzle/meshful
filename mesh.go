25e3175ed55e0e2423b0d12c1c1fa596c208574e
rknizzle <ryan@autotiv.com>
2020-05-28 23:29:20 -0400

    Added a function to get the bounding box of a mesh

--- mesh.go
+++ mesh.go
@@ -4,3 +4,38 @@ package meshful
 type Mesh struct {
 	Triangles []Triangle
 }
+
+func (mesh *Mesh) GetBoundingBox() [3]float32 {
+	minX := mesh.Triangles[0].Vertices[0].X
+	maxX := mesh.Triangles[0].Vertices[0].X
+	minY := mesh.Triangles[0].Vertices[0].Y
+	maxY := mesh.Triangles[0].Vertices[0].Y
+	minZ := mesh.Triangles[0].Vertices[0].Z
+	maxZ := mesh.Triangles[0].Vertices[0].Z
+
+	// loop through every vertex in the mesh
+	for _, triangle := range mesh.Triangles {
+		for _, vert := range triangle.Vertices {
+			if vert.X < minX {
+				minX = vert.X
+			}
+			if vert.X > maxX {
+				maxX = vert.X
+			}
+			if vert.Y < minY {
+				minY = vert.Y
+			}
+			if vert.Y > maxY {
+				maxY = vert.Y
+			}
+			if vert.Z < minZ {
+				minZ = vert.Z
+			}
+			if vert.Z > maxZ {
+				maxZ = vert.Z
+			}
+		}
+	}
+
+	return [3]float32{maxX - minX, maxY - minY, maxZ - minZ}
+}
